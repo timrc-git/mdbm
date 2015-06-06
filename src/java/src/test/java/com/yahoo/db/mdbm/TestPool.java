002cf336292b615c844cbd8dfde5d0ea1c6a588d
Allen Reese <areese@yahoo-inc.com>
2015-06-05 18:50:25 -0700

    Add further tests for the pooling issue we've been seeing

--- src/java/src/test/java/com/yahoo/db/mdbm/TestPool.java
+++ src/java/src/test/java/com/yahoo/db/mdbm/TestPool.java
@@ -1,6 +1,6 @@
 package com.yahoo.db.mdbm;
 
-import static com.yahoo.db.mdbm.TestSimpleMdbm.testMdbmV3Path;
+import static com.yahoo.db.mdbm.TestSimpleMdbm.fetchMdbmV3Path;
 
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -8,16 +8,16 @@ import org.testng.annotations.Test;
 public class TestPool {
 
 	@Test
-	public void testEmptyFirst() throws Exception {
+	public static void testEmptyFirst() throws Exception {
 		MdbmPoolInterface mdbmPool = null;
 		try {
-			mdbmPool = MdbmProvider.openPool(testMdbmV3Path,
+			mdbmPool = MdbmProvider.openPool(fetchMdbmV3Path,
 					Open.MDBM_O_RDONLY, 0755, 0, 0, 5);
 			try (MdbmInterface handle = mdbmPool.getMdbmHandle()) {
 				String key = "testkey";
 				MdbmDatum datum = new MdbmDatum(key.getBytes("UTF-8"));
 				MdbmDatum value = handle.fetch(datum);
-				Assert.assertNull(value);
+				Assert.assertNotNull(value);
 				mdbmPool.returnMdbmHandle(handle);
 			}
 
@@ -26,5 +26,9 @@ public class TestPool {
 				mdbmPool.close();
 			}
 		}
+	}
+	
+	public static void main(String[] args) throws Exception {
+		testEmptyFirst();
 	}
 }
--- src/java/src/test/java/com/yahoo/db/mdbm/TestSimpleMdbm.java
+++ src/java/src/test/java/com/yahoo/db/mdbm/TestSimpleMdbm.java
@@ -24,127 +24,125 @@ import com.yahoo.db.mdbm.exceptions.MdbmException;
 import com.yahoo.db.mdbm.exceptions.MdbmNoEntryException;
 
 public abstract class TestSimpleMdbm {
-	public static final String testMdbmV3Path = "test/resources/testv3.mdbm";
-	public static final String createMdbmV3Path = "target/create_testv3.mdbm";
-	public static final String fetchMdbmV3Path = "target/fetch_testv3.mdbm";
-	public static final String fetchMdbmV3PathSingleArch = "target/fetch_testv3_single_arch.mdbm";
-	public static final String iteratorMdbmV3PathA = "target/iterator_testv3.mdbm";
+    public static final String testMdbmV3Path = "test/resources/testv3.mdbm";
+    public static final String createMdbmV3Path = "target/create_testv3.mdbm";
+    public static final String fetchMdbmV3Path = "target/fetch_testv3.mdbm";
+    public static final String fetchMdbmV3PathSingleArch = "target/fetch_testv3_single_arch.mdbm";
+    public static final String iteratorMdbmV3PathA = "target/iterator_testv3.mdbm";
 
-	@Test(dataProvider = "createMdbms")
-	public static MdbmInterface testCreate(String path, int flags, boolean close)
-			throws MdbmException {
-		MdbmInterface mdbm = null;
+    @Test(dataProvider = "createMdbms")
+    public static MdbmInterface testCreate(String path, int flags, boolean close) throws MdbmException {
+        MdbmInterface mdbm = null;
 
-		String key = "abc";
-		String value = "def";
-		try {
-			File f = new File(path);
-			f.delete();
-			Assert.assertFalse(f.exists());
+        String key = "abc";
+        String value = "def";
+        try {
+            File f = new File(path);
+            f.delete();
+            Assert.assertFalse(f.exists());
 
-			mdbm = MdbmProvider.open(path, flags, 0755, 0, 0);
-			Assert.assertFalse(mdbm.isClosed());
+            mdbm = MdbmProvider.open(path, flags, 0755, 0, 0);
+            Assert.assertFalse(mdbm.isClosed());
 
-			mdbm.storeString(key, value, Store.MDBM_REPLACE);
-			String ret = mdbm.fetchString(key);
-			Assert.assertEquals(ret, value);
-			try {
-				ret = mdbm.fetchString("nothere");
-			} catch (MdbmNoEntryException e) {
-			}
+            mdbm.storeString(key, value, Store.MDBM_REPLACE);
+            String ret = mdbm.fetchString(key);
+            Assert.assertEquals(ret, value);
+            try {
+                ret = mdbm.fetchString("nothere");
+            } catch (MdbmNoEntryException e) {
+            }
 
-			return mdbm;
-		} finally {
-			if (close && null != mdbm)
-				mdbm.close();
-		}
+            return mdbm;
+        } finally {
+            if (close && null != mdbm)
+                mdbm.close();
+        }
 
-	}
+    }
 
-	@Test(dataProvider = "versions")
-	public void testVersion(String path) throws MdbmException {
-		MdbmInterface mdbm = null;
-		try {
-			mdbm = MdbmProvider.open(path, Open.MDBM_O_RDONLY, 0755, 0, 0);
-		} catch (java.lang.AssertionError ae) {
-			dumpStats(path);
-			throw ae;
-		} finally {
-			if (null != mdbm)
-				mdbm.close();
-		}
-	}
+    @Test(dataProvider = "versions")
+    public void testVersion(String path) throws MdbmException {
+        MdbmInterface mdbm = null;
+        try {
+            mdbm = MdbmProvider.open(path, Open.MDBM_O_RDONLY, 0755, 0, 0);
+        } catch (java.lang.AssertionError ae) {
+            dumpStats(path);
+            throw ae;
+        } finally {
+            if (null != mdbm)
+                mdbm.close();
+        }
+    }
 
-	@Test
-	public void testEmptyFirst() throws MdbmException {
-		MdbmInterface mdbm = null;
-		MdbmIterator iter = null;
-		try {
-			mdbm = MdbmProvider.open(testMdbmV3Path, Open.MDBM_O_RDONLY, 0755,
-					0, 0);
-			iter = mdbm.iterator();
+    @Test
+    public void testEmptyFirst() throws MdbmException {
+        MdbmInterface mdbm = null;
+        MdbmIterator iter = null;
+        try {
+            mdbm = MdbmProvider.open(testMdbmV3Path, Open.MDBM_O_RDONLY, 0755, 0, 0);
+            iter = mdbm.iterator();
 
-			Assert.assertNotNull(iter);
+            Assert.assertNotNull(iter);
 
-			MdbmKvPair kv = mdbm.first(iter);
+            MdbmKvPair kv = mdbm.first(iter);
 
-			Assert.assertNull(kv);
-			kv = mdbm.next(iter);
+            Assert.assertNull(kv);
+            kv = mdbm.next(iter);
 
-			Assert.assertNull(kv);
-		} finally {
-			if (null != mdbm)
-				mdbm.close();
-			if (null != iter)
-				iter.close();
-		}
-	}
+            Assert.assertNull(kv);
+        } finally {
+            if (null != mdbm)
+                mdbm.close();
+            if (null != iter)
+                iter.close();
+        }
+    }
 
-	@Test(dataProvider = "iterateMdbms")
-	public static void testIterate(String path, int flags) throws MdbmException {
-		MdbmInterface mdbm = null;
+    @Test(dataProvider = "iterateMdbms")
+    public static void testIterate(String path, int flags) throws MdbmException {
+        MdbmInterface mdbm = null;
 
-		try {
-			mdbm = testCreate(path, flags, false);
-			int max = 10;
-			for (int i = 0; i < max; i++) {
-				String s = Integer.toString(i);
-				mdbm.storeString(s, s, 0);
-			}
-			for (int i = 0; i < max; i++) {
-				String k = Integer.toString(i);
-				String v = mdbm.fetchString(k);
-				Assert.assertNotNull(v);
-			}
-			String k = Integer.toString(max);
-			String v = mdbm.fetchString(k);
-			Assert.assertNull(v);
-		} finally {
-			if (null != mdbm)
-				mdbm.close();
-		}
-	}
+        try {
+            mdbm = testCreate(path, flags, false);
+            int max = 10;
+            for (int i = 0; i < max; i++) {
+                String s = Integer.toString(i);
+                mdbm.storeString(s, s, 0);
+            }
+            for (int i = 0; i < max; i++) {
+                String k = Integer.toString(i);
+                String v = mdbm.fetchString(k);
+                Assert.assertNotNull(v);
+            }
+            String k = Integer.toString(max);
+            String v = mdbm.fetchString(k);
+            Assert.assertNull(v);
+        } finally {
+            if (null != mdbm)
+                mdbm.close();
+        }
+    }
 
-	@Test
-	public void testFetchString() throws MdbmException,
-			UnsupportedEncodingException {
-		String key = "testkey";
-		MdbmDatum datum = new MdbmDatum(key.getBytes("UTF-8"));
-		MdbmInterface mdbm = null;
-		try {
-			mdbm = MdbmProvider.open(fetchMdbmV3Path, Open.MDBM_CREATE_V3
-					| Open.MDBM_O_RDWR | Open.MDBM_O_CREAT, 0755, 0, 0);
-			mdbm.plock(datum, 0);
-			mdbm.storeString(key, key, Constants.MDBM_REPLACE);
-			mdbm.punlock(datum, 0);
-			Assert.assertEquals(mdbm.fetchString(key), key);
-		} finally {
-			if (null != mdbm)
-				mdbm.close();
-		}
-	}
+    @Test
+    public void testFetchString() throws MdbmException, UnsupportedEncodingException {
+        String key = "testkey";
+        MdbmDatum datum = new MdbmDatum(key.getBytes("UTF-8"));
+        MdbmInterface mdbm = null;
+        try {
+            mdbm =
+                            MdbmProvider.open(fetchMdbmV3Path, Open.MDBM_CREATE_V3 | Open.MDBM_O_RDWR
+                                            | Open.MDBM_O_CREAT, 0755, 0, 0);
+            mdbm.plock(datum, 0);
+            mdbm.storeString(key, key, Constants.MDBM_REPLACE);
+            mdbm.punlock(datum, 0);
+            Assert.assertEquals(mdbm.fetchString(key), key);
+        } finally {
+            if (null != mdbm)
+                mdbm.close();
+        }
+    }
 
-	@Test
+    @Test
     public void testFetch() throws MdbmException, UnsupportedEncodingException {
         String key = "testkey";
         MdbmDatum datum = new MdbmDatum(key.getBytes("UTF-8"));
@@ -167,7 +165,7 @@ public abstract class TestSimpleMdbm {
         }
     }
 
-	@Test
+    @Test
     public void testFetchWithoutIterator() throws MdbmException, UnsupportedEncodingException {
         String key = "testkey";
         MdbmDatum datum = new MdbmDatum(key.getBytes("UTF-8"));
@@ -190,30 +188,29 @@ public abstract class TestSimpleMdbm {
         }
     }
 
-	public static void dumpStats(String file) {
-		try {
-			// ProcessBuilder pb = new ProcessBuilder("mdbm_stat",
-			// "-H", file);
-			// pb.redirectErrorStream(true);
-			// Process p = pb.start();
-			Process p = Runtime.getRuntime().exec("mdbm_stat" + " -H " + file);
+    public static void dumpStats(String file) {
+        try {
+            // ProcessBuilder pb = new ProcessBuilder("mdbm_stat",
+            // "-H", file);
+            // pb.redirectErrorStream(true);
+            // Process p = pb.start();
+            Process p = Runtime.getRuntime().exec("mdbm_stat" + " -H " + file);
 
-			Thread.sleep(1000);
-			BufferedReader isr = new BufferedReader(new InputStreamReader(
-					new BufferedInputStream(p.getInputStream())));
-			String line = null;
-			while ((line = isr.readLine()) != null) {
-				System.err.println(line);
-			}
+            Thread.sleep(1000);
+            BufferedReader isr = new BufferedReader(new InputStreamReader(new BufferedInputStream(p.getInputStream())));
+            String line = null;
+            while ((line = isr.readLine()) != null) {
+                System.err.println(line);
+            }
 
-			p.exitValue();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+            p.exitValue();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
 
-	}
+    }
 }
